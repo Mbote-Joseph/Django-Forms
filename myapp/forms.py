be8482659801317b9d418e2b29ec03f9be1dea39
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-19 12:55:44 +0300

    Django Forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -12,3 +12,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .models import Details
+from django.forms import ModelForm
+from django import forms
+
+class DetailsForm(ModelForm):
+    class Meta:
+        model = Details
+        fields = "__all__"
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'name': 'Name',
+            'email': 'Email',
+            'phone': 'Phone',
+            'message': 'Message',
+        }
+        help_texts = {
+            'name': '',
+            'email': '',
+            'phone': '',
+            'message': '',
+        }
+        error_messages = {
+            'name': {
+                'required': 'Name is required.',
+            },
+            'email': {
+                'required': 'Email is required.',
+            },
+            'phone': {
+                'required': 'Phone is required.',
+            },
+            'message': {
+                'required': 'Message is required.',
+            },
+        }
+        field_classes = {
+            'name': forms.CharField,
+            'email': forms.EmailField,
+            'phone': forms.CharField,
+            'message': forms.CharField,
+        }
+        localized_fields = {
+            'name': 'name',
+            'email': 'email',
+            'phone': 'phone',
+            'message': 'message',
+        }
+        localized_labels = {
+            'name': 'Name',
+            'email': 'Email',
+            'phone': 'Phone',
+            'message': 'Message',
+        }
+        localized_help_texts = {
+            'name': '',
+            'email': '',
+            'phone': '',
+            'message': '',
+        }
+        
