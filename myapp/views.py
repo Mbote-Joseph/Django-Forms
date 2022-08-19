2fcbe8b45cbeacf3e462334bc44dbfb625445088
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-19 13:03:45 +0300

    Django Forms

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from .models import Details
+from .forms import DetailsForm
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        form = DetailsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = DetailsForm()
+    else:
+        form = DetailsForm()
+    
+
     context={
         'title':'Django Forms',
+        'form': form,
+        "detailform":DetailsForm()
     }
     return render(request, 'index.html', context)
